2a79ef0dafc8333e3ff08b426d58a5731471f056
annasaharova <sugarcaramel@yandex.ru>
2021-05-27 22:41:43 +0300

    commit

--- src/main/java/com/spring/spring_boot/service/UserDetailsServiceImpl.java
+++ src/main/java/com/spring/spring_boot/service/UserDetailsServiceImpl.java
@@ -14,9 +14,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         this.userService = userService;
     }
 
-    // «Пользователь» – это просто Object. В большинстве случаев он может быть
-    //  приведен к классу UserDetails.
-    // Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return userService.findByUserName(s);
